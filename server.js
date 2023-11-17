96534f813dfa10e7c041583001780fbd55e2e511
unknown <davgio7@gmail.com>
2023-11-17 15:38:14 +0000

    add new db

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 4000;
 
 mongoose.set('strictQuery', true);
 mongoose
-  .connect(`${process.env.DB_CONNECTION_URL}`)
+  .connect(`${process.env.MONGODB_URI}`)
   .then(console.log("db connected"))
   .catch((err) => console.log(err.message));
 
